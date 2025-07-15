c435ad5a86634584cd02ad090147734def0031b9
Ashan Dissanayake <ashanpathum899@gmail.com>
2025-07-15 09:41:10 +0530

    Add Spring Boot server application
    - Initial Spring Boot project structure
    
    - Maven configuration with dependencies
    
    - Application properties setup
    
    - Basic package structure created

---
+++ NTCServerApllication/src/main/java/lk/ashan/ntcserverapllication/NtcServerApllicationApplication.java
@@ -0,0 +1,13 @@
+package lk.ashan.ntcserverapllication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtcServerApllicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtcServerApllicationApplication.class, args);
+	}
+
+}
---
+++ NTCServerApllication/src/test/java/lk/ashan/ntcserverapllication/NtcServerApllicationApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ashan.ntcserverapllication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtcServerApllicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
