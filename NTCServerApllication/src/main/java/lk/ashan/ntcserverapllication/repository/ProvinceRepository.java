a9290eec2902b8d2127c4b3f119b4d3e2fabbaae
Ashan Dissanayake <ashanpathum899@gmail.com>
2025-07-15 20:06:45 +0530

    Implementing repository classes

---
+++ NTCServerApllication/src/main/java/lk/ashan/ntcserverapllication/repository/ProvinceRepository.java
@@ -0,0 +1,9 @@
+package lk.ashan.ntcserverapllication.repository;
+
+import lk.ashan.ntcserverapllication.model.entity.Province;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProvinceRepository extends JpaRepository<Province, Integer> {
+}
