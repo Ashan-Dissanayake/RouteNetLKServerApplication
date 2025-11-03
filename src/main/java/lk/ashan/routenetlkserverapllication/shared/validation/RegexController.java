bca812fa394b65ee998961c662005dc10fb17973
Ashan Dissanayake <ashanpathum899@gmail.com>
2025-11-03 19:09:51 +0530

    Define new endpoint in regex controller for employee

--- src/main/java/lk/ashan/routenetlkserverapllication/shared/validation/RegexController.java
+++ src/main/java/lk/ashan/routenetlkserverapllication/shared/validation/RegexController.java
@@ -1,6 +1,7 @@
 package lk.ashan.routenetlkserverapllication.shared.validation;
 
 import lk.ashan.routenetlkserverapllication.module.branch.dto.BranchRequestDto;
+import lk.ashan.routenetlkserverapllication.module.employee.dto.EmployeeRequestDto;
 import lk.ashan.routenetlkserverapllication.shared.api.APIResponseBuilder;
 import lk.ashan.routenetlkserverapllication.shared.api.dto.APISuccessResponse;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,13 @@ public class RegexController {
     @GetMapping(path ="/branches", produces = "application/json")
     public ResponseEntity<APISuccessResponse<HashMap<String,HashMap<String,String>>>> branch() {
        HashMap<String,HashMap<String,String>> regexes =  RegexProvider.get(new BranchRequestDto());
+        assert regexes != null;
+        return APIResponseBuilder.getResponse(regexes, regexes.size());
+    }
+
+    @GetMapping(path ="/employees", produces = "application/json")
+    public ResponseEntity<APISuccessResponse<HashMap<String,HashMap<String,String>>>> employee() {
+       HashMap<String,HashMap<String,String>> regexes =  RegexProvider.get(new EmployeeRequestDto());
         assert regexes != null;
         return APIResponseBuilder.getResponse(regexes, regexes.size());
     }
