bf7898fe62caaf27798e21f617aea45aaff11082
Ashan Dissanayake <ashanpathum899@gmail.com>
2025-11-23 11:59:03 +0530

    Implement conitionrate end point and test

---
+++ src/main/java/lk/ashan/routenetlkserverapllication/module/vehicle/controller/ConditionrateController.java
@@ -0,0 +1,30 @@
+package lk.ashan.routenetlkserverapllication.module.vehicle.controller;
+
+import lk.ashan.routenetlkserverapllication.module.vehicle.dto.ConditionrateDto;
+import lk.ashan.routenetlkserverapllication.module.vehicle.service.ConditionrateService;
+import lk.ashan.routenetlkserverapllication.shared.api.APIResponseBuilder;
+import lk.ashan.routenetlkserverapllication.shared.api.dto.APISuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/conditionrates")
+@RequiredArgsConstructor
+public class ConditionrateController {
+
+    private final ConditionrateService conditionrateService;
+
+    @GetMapping(path ="/list", produces = "application/json")
+    public ResponseEntity<APISuccessResponse<List<ConditionrateDto>>> get() {
+        List<ConditionrateDto> conditionrates = conditionrateService.getConditionRates();
+        return APIResponseBuilder.getResponse(conditionrates, conditionrates.size());
+    }
+
+}
---
+++ src/main/java/lk/ashan/routenetlkserverapllication/module/vehicle/service/ConditionrateService.java
@@ -0,0 +1,22 @@
+package lk.ashan.routenetlkserverapllication.module.vehicle.service;
+
+import lk.ashan.routenetlkserverapllication.module.vehicle.dto.ConditionrateDto;
+import lk.ashan.routenetlkserverapllication.module.vehicle.mapper.ConditionrateMapper;
+import lk.ashan.routenetlkserverapllication.module.vehicle.repository.ConditionrateRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ConditionrateService {
+
+    private final ConditionrateRepository conditionrateRepository;
+    private final ConditionrateMapper conditionrateMapper;
+
+    public List<ConditionrateDto> getConditionRates(){
+       return conditionrateMapper.toDtoList(conditionrateRepository.findAll());
+    }
+
+}
