d5c9ad2dec8f58bb19b5334bf336e773700f5166
Ashan Dissanayake <ashanpathum899@gmail.com>
2025-11-23 12:29:15 +0530

    Implement fueltyp endpoint and test

---
+++ src/main/java/lk/ashan/routenetlkserverapllication/module/vehicle/controller/FueltypeController.java
@@ -0,0 +1,30 @@
+package lk.ashan.routenetlkserverapllication.module.vehicle.controller;
+
+import lk.ashan.routenetlkserverapllication.module.vehicle.dto.FueltypeDto;
+import lk.ashan.routenetlkserverapllication.module.vehicle.service.FueltypeService;
+import lk.ashan.routenetlkserverapllication.shared.api.APIResponseBuilder;
+import lk.ashan.routenetlkserverapllication.shared.api.dto.APISuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/fueltypes")
+@RequiredArgsConstructor
+public class FueltypeController {
+
+    private final FueltypeService fueltypeService;
+
+    @GetMapping(path ="/list", produces = "application/json")
+    public ResponseEntity<APISuccessResponse<List<FueltypeDto>>> get() {
+        List<FueltypeDto> fueltypes = fueltypeService.getFueltypes();
+        return APIResponseBuilder.getResponse(fueltypes, fueltypes.size());
+    }
+
+}
