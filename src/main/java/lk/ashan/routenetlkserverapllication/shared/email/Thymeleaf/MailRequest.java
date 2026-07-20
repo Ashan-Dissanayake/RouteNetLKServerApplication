06e3052be1c6d3789c0d69b222dd1df356314c00
Ashan Dissanayake <ashanpathum899@gmail.com>
2026-07-20 23:27:06 +0530

    implement email service

---
+++ src/main/java/lk/ashan/routenetlkserverapllication/shared/email/Thymeleaf/MailRequest.java
@@ -0,0 +1,16 @@
+package lk.ashan.routenetlkserverapllication.shared.email.Thymeleaf;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MailRequest {
+
+    private String to;
+    private String subject;
+    private String recipientName;
+    private String content;
+    private String signature;
+    private String templateName;
+}
