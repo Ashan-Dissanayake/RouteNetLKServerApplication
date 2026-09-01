69b0c3c30f8250e8668ff4ea250557ccf967ef84
Ashan Dissanayake <ashanpathum899@gmail.com>
2026-09-01 12:48:37 +0530

    make changes of deploy.yml to avoid make unwanted cicd executions

--- src/main/java/lk/ashan/routenetlkserverapllication/RouteNetLKServerApplication.java
+++ src/main/java/lk/ashan/routenetlkserverapllication/RouteNetLKServerApplication.java
@@ -1,17 +1,7 @@
 package lk.ashan.routenetlkserverapllication;
 
-import lk.ashan.routenetlkserverapllication.module.user.model.entity.User;
-import lk.ashan.routenetlkserverapllication.security.CustomUserPrincipal;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.util.Optional;
 
 @SpringBootApplication
 //@EnableJpaAuditing(auditorAwareRef = "auditorAware")
