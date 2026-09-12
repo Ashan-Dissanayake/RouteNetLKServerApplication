77b3203280928d0444e8d548f9fbbbc0d2d894ce
Ashan Dissanayake <ashanpathum899@gmail.com>
2026-09-12 12:02:57 +0530

    feat: setup spring environment profiles and sync automated tests in CI/CD pipeline

--- src/test/java/lk/ashan/routenetlkserverapllication/RouteNetLKServerApplicationTests.java
+++ src/test/java/lk/ashan/routenetlkserverapllication/RouteNetLKServerApplicationTests.java
@@ -1,13 +1,17 @@
 package lk.ashan.routenetlkserverapllication;
 
+import lk.ashan.routenetlkserverapllication.shared.config.BaseTestContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class RouteNetLKServerApplicationTests {
+@ActiveProfiles("test")
+class RouteNetLKServerApplicationTests extends BaseTestContainer {
 
 	@Test
 	void contextLoads() {
 	}
 
 }
+
